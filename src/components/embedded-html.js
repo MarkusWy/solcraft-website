41266b053230b4b27833350790b18a19ba75048e
MarkusWy <solbrilla1@gmail.com>
2022-10-18 01:47:53 +0200

    added console log

--- src/components/embedded-html.js
+++ src/components/embedded-html.js
@@ -5,6 +5,7 @@ import { Container, Section, Text, SuperHeading } from "./ui"
 import * as styles from "./about-hero.css"
 
 export default function EmbeddedHtml(props) {
+  console.log(props);
   return (
     <Section>
       <Container>
